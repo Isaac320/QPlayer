53e6c8bafcf3ac980865ecb4ef8a18c5c83d1da6
Isaac Q <943339676@qq.com>
2018-06-27 14:55:16 +0800

    先这样吧

---
+++ QPlayer/QPlayer/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace QPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.58197F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.41803F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(757, 488);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(751, 397);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 406);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(751, 79);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 488);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ QPlayer/QPlayer/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace QPlayer
+{
+    public partial class Form1 : Form
+    {
+        string args = "";
+
+        /// <summary>
+        /// Path of selected file
+        /// </summary>
+        string filename = null;
+        Process ps = null;
+        public Form1()
+        {
+            InitializeComponent();
+            ps = new Process();
+
+            //Path of Mplayer exe
+            ps.StartInfo.FileName = "mplayer ";
+
+            ps.StartInfo.UseShellExecute = false;
+            ps.StartInfo.RedirectStandardInput = true;
+            args = "-nofs -noquiet -identify -slave ";
+            args += "-nomouseinput -sub-fuzziness 1 ";
+
+            //-wid will tell MPlayer to show output inisde our panel
+            args += " -vo direct3d, -ao dsound  -wid ";
+            int id = (int)panel1.Handle;
+            args += id;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            if(opd.ShowDialog()==DialogResult.OK)
+            {
+                filename = opd.FileName;
+                OpenFile();
+            }
+        }
+        bool SendCommand(string cmd)
+        {
+            try
+            {
+                if (ps != null && ps.HasExited == false)
+                {
+                    ps.StandardInput.Write(cmd + "\n");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        void OpenFile()
+        {
+            if (filename == null)
+                return;
+            //Close any current playing media file
+            try
+            {
+                ps.Kill();
+            }
+            catch
+            {
+            }
+            try
+            {
+                ps.StartInfo.Arguments = args + " \"" + filename + "\"";
+                ps.Start();
+              //  SendCommand("set_property volume " + trkVolume.Value);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (SendCommand("pause") == false)
+            {
+                OpenFile();
+
+            }
+        }
+    }
+}
